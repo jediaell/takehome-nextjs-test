2b09da29767f50b31da6d900ace4d2511068e968
Jediael Ferreira de Oliveira Fonseca <jediael@MacBook-Pro-de-Admin-A2B.local>
2022-04-27 16:59:26 -0300

    fix: image hostname error

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
-/** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
+  images: {
+    domains: ['assets.vercel.com'],
+    formats: ['image/avif', 'image/webp'],
+  },
 }
